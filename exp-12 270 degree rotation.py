c7165a1a38c8c862520def1db3e54233fd158eb3
saijagadeesh-88 <192472146.simats@saveetha.com>
2026-03-04 15:30:43 +0530

    Update and rename exp-12 270 degree rotation to exp-12 270 degree rotation.py

--- exp-12 270 degree rotation.py
+++ exp-12 270 degree rotation.py
@@ -6,4 +6,5 @@ cv2.imshow("Original Image", image)
 cv2.imshow("Rotated 270-degree Clockwise Along Y-axis", rotated_image) 
 cv2.imwrite("rotated_image_270.jpg", rotated_image) 
 cv2.waitKey(0) 
+
 cv2.destroyAllWindows()
